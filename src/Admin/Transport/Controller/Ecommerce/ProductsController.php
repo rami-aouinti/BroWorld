45a2b842e8522330dcfacee8d5a47f789f9d5179
Rami Aouinti <raouinti@MacBook-Pro-von-Rami.local>
2023-09-21 22:50:43 +0200

    Fix Admin Features

--- src/Admin/Transport/Controller/Ecommerce/ProductsController.php
+++ src/Admin/Transport/Controller/Ecommerce/ProductsController.php
@@ -33,47 +33,26 @@ class ProductsController extends AbstractController
         PictureService $pictureService
     ): Response
     {
-        //On crée un "nouveau produit"
         $product = new Products();
-
-        // On crée le formulaire
         $productForm = $this->createForm(ProductsFormType::class, $product);
-
-        // On traite la requête du formulaire
         $productForm->handleRequest($request);
-
-        //On vérifie si le formulaire est soumis ET valide
         if($productForm->isSubmitted() && $productForm->isValid()){
             // On récupère les images
             $images = $productForm->get('images')->getData();
 
             foreach($images as $image){
-                // On définit le dossier de destination
                 $folder = 'products';
-
-                // On appelle le service d'ajout
                 $fichier = $pictureService->add($image, $folder, 300, 300);
-
                 $img = new Images();
                 $img->setName($fichier);
                 $product->addImage($img);
             }
-
-            // On génère le slug
             $slug = $slugger->slug($product->getName());
             $product->setSlug($slug);
-
-            // On arrondit le prix
-            // $prix = $product->getPrice() * 100;
-            // $product->setPrice($prix);
-
-            // On stocke
             $em->persist($product);
             $em->flush();
-
             $this->addFlash('success', 'Produit ajouté avec succès');
 
-            // On redirige
             return $this->redirectToRoute('ecommerce_admin_products_index');
         }
 
@@ -89,12 +68,10 @@ class ProductsController extends AbstractController
         PictureService $pictureService
     ): Response
     {
-
         $productForm = $this->createForm(ProductsFormType::class, $product);
         $productForm->handleRequest($request);
         if($productForm->isSubmitted() && $productForm->isValid()){
             $images = $productForm->get('images')->getData();
-
             foreach($images as $image){
                 $folder = 'products';
                 $fichier = $pictureService->add($image, $folder, 300, 300);
@@ -103,23 +80,12 @@ class ProductsController extends AbstractController
                 $img->setName($fichier);
                 $product->addImage($img);
             }
-
-
-            // On génère le slug
             $slug = $slugger->slug($product->getName());
             $product->setSlug($slug);
-
-            // On arrondit le prix
-            // $prix = $product->getPrice() * 100;
-            // $product->setPrice($prix);
-
-            // On stocke
             $em->persist($product);
             $em->flush();
-
             $this->addFlash('success', 'Produit modifié avec succès');
 
-            // On redirige
             return $this->redirectToRoute('ecommerce/admin_products_index');
         }
 
@@ -128,9 +94,6 @@ class ProductsController extends AbstractController
             'productForm' => $productForm->createView(),
             'product' => $product
         ]);
-
-        // return $this->renderForm('admin/products/edit.html.twig', compact('productForm'));
-        // ['productForm' => $productForm]
     }
 
     #[Route('/suppression/{id}', name: 'delete')]
@@ -145,16 +108,12 @@ class ProductsController extends AbstractController
     #[Route('/suppression/image/{id}', name: 'delete_image', methods: ['DELETE'])]
     public function deleteImage(Images $image, Request $request, EntityManagerInterface $em, PictureService $pictureService): JsonResponse
     {
-        // On récupère le contenu de la requête
         $data = json_decode($request->getContent(), true);
 
         if($this->isCsrfTokenValid('delete' . $image->getId(), $data['_token'])){
-            // Le token csrf est valide
-            // On récupère le nom de l'image
             $nom = $image->getName();
 
             if($pictureService->delete($nom, 'products', 300, 300)){
-                // On supprime l'image de la base de données
                 $em->remove($image);
                 $em->flush();
 
